954b41548a97fe510c981d47b01f9c571a76e1dc
Lasfolt <b03902071@ntu.edu.tw>
2017-01-02 21:51:47 +0800

    fix small bug

--- EliteChat/messenger/views.py
+++ EliteChat/messenger/views.py
@@ -3,14 +3,18 @@ from django.http import JsonResponse, HttpResponse
 from rest_framework.views import APIView, status
 from django.contrib.auth.decorators import login_required
 from .models import Message
+from django.contrib.auth.models import User
 import json
 import datetime
 
 class UserMessenger(APIView):
     def get(self, request, format=None):
         data = []
-        message_obj = Message.objects.all().filter(owner=request.user)
-        for obj in message_obj.iterator():
+        user = User.objects.get(username=request.user)
+        print(request.user)
+        for obj in user.owned_message.all().iterator():
+            data.append({"content" : obj.content, "time": obj.time})
+        for obj in user.received_message.all().iterator():
             data.append({"content" : obj.content, "time": obj.time})
         print(data)
         return HttpResponse(data)
