c975b2c0a9303d3b561c7c8f004d1504b90a778d
bookgin <dongsheoil@gmail.com>
2017-01-14 11:39:27 +0800

    Ready to deploy

--- EliteChat/settings.py
+++ EliteChat/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8ol%k4ap9l!fuli#ax6hgvt_oj*zs66*4y=y*tbd$ox%x8$a&-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,6 +122,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-#STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/'
